e8cbedbed24af9e5a3b948ebb9ab68832a10e2f2
Your Name <okedijimicheal1@gmail.com>
2025-03-13 00:52:50 -0700

    new with pictures

--- index.js
+++ index.js
@@ -15,164 +15,164 @@ mongoose
   });
 const cities = [
   {
-    "name": "New York City",
+    "name": "New York",
     "population": 8419600,
     "country": "United States",
-    "landmass": 468.9,
+    "landmass": "New York",
     "landmark": "Statue of Liberty",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/1/1f/Statue_of_Liberty_7.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/a/a1/Statue_of_Liberty_7.jpg"
   },
   {
     "name": "Los Angeles",
     "population": 3980400,
     "country": "United States",
-    "landmass": 503,
+    "landmass": "California",
     "landmark": "Hollywood Sign",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/4/45/Hollywood_Sign_2015.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/9/91/Hollywood_Sign_%28Santa_Monica_Mountains%29.jpg"
   },
   {
     "name": "Chicago",
     "population": 2716000,
     "country": "United States",
-    "landmass": 234,
-    "landmark": "Willis Tower",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/c/c8/Willis_Tower_from_the_north_2016.jpg"
+    "landmass": "Illinois",
+    "landmark": "Willis Tower (Sears Tower)",
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/4/44/Willis_Tower_Chicago_2016.jpg"
   },
   {
     "name": "Houston",
     "population": 2328000,
     "country": "United States",
-    "landmass": 637.4,
+    "landmass": "Texas",
     "landmark": "Space Center Houston",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/6/6f/Space_Center_Houston_2.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/4/42/Space_Center_Houston_-_Space_Suit_Collection.jpg"
   },
   {
     "name": "Phoenix",
     "population": 1690000,
     "country": "United States",
-    "landmass": 517.6,
+    "landmass": "Arizona",
     "landmark": "Desert Botanical Garden",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/a/a5/Desert_Botanical_Garden_-_Phoenix%2C_Arizona_%282012%29.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/4/4d/Desert_Botanical_Garden.jpg"
   },
   {
     "name": "Philadelphia",
     "population": 1584200,
     "country": "United States",
-    "landmass": 142.6,
+    "landmass": "Pennsylvania",
     "landmark": "Liberty Bell",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/0/06/Liberty_Bell_2008.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/4/47/Liberty_Bell_2008.jpg"
   },
   {
     "name": "San Antonio",
     "population": 1547000,
     "country": "United States",
-    "landmass": 461.0,
+    "landmass": "Texas",
     "landmark": "The Alamo",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/0/02/Alamo_-_San_Antonio.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/7/77/The_Alamo_2019.jpg"
   },
   {
     "name": "San Diego",
-    "population": 1425000,
+    "population": 1423851,
     "country": "United States",
-    "landmass": 372.4,
-    "landmark": "USS Midway Museum",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/0/0e/USS_Midway_Museum_%28San_Diego%29.jpg"
+    "landmass": "California",
+    "landmark": "Balboa Park",
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/0/0c/Balboa_Park_%28San_Diego%29_%28a%29.jpg"
   },
   {
     "name": "Dallas",
-    "population": 1341000,
+    "population": 1340000,
     "country": "United States",
-    "landmass": 343.3,
-    "landmark": "Dallas Cowboys Stadium",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/d/d3/AT%26T_Stadium_%28January_2013%29.jpg"
+    "landmass": "Texas",
+    "landmark": "Dealey Plaza",
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/4/43/Dealey_Plaza_-_Dallas_-_Texas.jpg"
   },
   {
     "name": "San Jose",
     "population": 1027000,
     "country": "United States",
-    "landmass": 179.97,
+    "landmass": "California",
     "landmark": "Winchester Mystery House",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/5/5e/Winchester_Mystery_House%2C_San_Jose%2C_CA.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/7/79/Winchester_Mystery_House%2C_2019.jpg"
   },
   {
     "name": "Austin",
     "population": 964000,
     "country": "United States",
-    "landmass": 271.8,
+    "landmass": "Texas",
     "landmark": "Texas State Capitol",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/e/ef/Texas_State_Capitol_-_Austin%2C_Texas_%282019%29.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/4/44/Texas_State_Capitol_2018.jpg"
   },
   {
     "name": "Jacksonville",
     "population": 911507,
     "country": "United States",
-    "landmass": 874.6,
-    "landmark": "Cummer Museum of Art and Gardens",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/4/46/Cummer_Museum_entrance_%28cropped%29.jpg"
+    "landmass": "Florida",
+    "landmark": "Jacksonville Beach",
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/3/32/Jacksonville_Beach.JPG"
   },
   {
     "name": "Fort Worth",
-    "population": 918915,
+    "population": 927000,
     "country": "United States",
-    "landmass": 349.6,
+    "landmass": "Texas",
     "landmark": "Fort Worth Stockyards",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/0/01/Fort_Worth_Stockyards_%28fortworth.com%29.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/a/a0/Fort_Worth_Stockyards_2.jpg"
   },
   {
     "name": "Columbus",
-    "population": 909000,
+    "population": 905748,
     "country": "United States",
-    "landmass": 217.2,
-    "landmark": "Ohio Statehouse",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/a/a6/Ohio_Statehouse_Capitol_Building_Columbus.jpg"
+    "landmass": "Ohio",
+    "landmark": "LeVeque Tower",
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/9/99/LeVeque_Tower_-_Columbus%2C_Ohio.jpg"
   },
   {
     "name": "Indianapolis",
     "population": 876384,
     "country": "United States",
-    "landmass": 368.2,
+    "landmass": "Indiana",
     "landmark": "Indianapolis Motor Speedway",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/4/47/Indianapolis_Motor_Speedway_%28open_oval%29.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/1/1b/Indianapolis_Motor_Speedway_2015.jpg"
   },
   {
     "name": "Charlotte",
-    "population": 885000,
+    "population": 874579,
     "country": "United States",
-    "landmass": 308.6,
-    "landmark": "Carowinds",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/e/ea/Carowinds_entrance.jpg"
+    "landmass": "North Carolina",
+    "landmark": "NASCAR Hall of Fame",
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/0/0a/NASCAR_Hall_of_Fame.jpg"
   },
   {
     "name": "Seattle",
-    "population": 744955,
+    "population": 753675,
     "country": "United States",
-    "landmass": 83.9,
+    "landmass": "Washington",
     "landmark": "Space Needle",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/a/a7/Seattle_Space_Needle_2018.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/a/aa/Space_Needle_%28Seattle%29_%28cropped%29.jpg"
   },
   {
     "name": "Denver",
     "population": 715522,
     "country": "United States",
-    "landmass": 154.7,
-    "landmark": "Red Rocks Amphitheatre",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/9/98/Red_Rocks_Amphitheatre_%28cropped%29.jpg"
+    "landmass": "Colorado",
+    "landmark": "Denver Botanic Gardens",
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/6/63/Denver_Botanic_Gardens_01.jpg"
   },
   {
-    "name": "Washington, D.C.",
+    "name": "Washington D.C.",
     "population": 705749,
     "country": "United States",
-    "landmass": 68.3,
-    "landmark": "Lincoln Memorial",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/a/a8/Lincoln_Memorial_%28Washington%29.jpg"
+    "landmass": "District of Columbia",
+    "landmark": "National Mall",
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/6/6f/National_Mall_in_Washington%2C_D.C.%2C_looking_south.jpg"
   },
   {
     "name": "Boston",
     "population": 692600,
     "country": "United States",
-    "landmass": 48.4,
+    "landmass": "Massachusetts",
     "landmark": "Fenway Park",
-    "picture": "https://upload.wikimedia.org/wikipedia/commons/c/cf/Fenway_Park_2015.jpg"
+    "picture": "https://upload.wikimedia.org/wikipedia/commons/c/c3/Fenway_Park_-_Boston%2C_Massachusetts_-_03.jpg"
   }
 ]
 ;
